c033e3b73936df402002cc1e5386f4016333af6b
CaioAlmeida09 <Caio09092001@gmail.com>
2023-07-22 11:58:19 -0300

    atualizando minhas rotas node

--- index.js
+++ index.js
@@ -1,61 +1,77 @@
-const express = require("express")
-const app = express()
-const uuid = require("uuid")
-app.use(express.json())
-app.listen(3000)
+const express = require("express");
+const app = express();
+const uuid = require("uuid");
+app.use(express.json());
+const cors = require('cors')
+app.use(cors())
 
-const pedidos = []
-const pedidEspc = []
-app.post('/pedido', (request, response) => {
-    const firstpedido = request.body
-    const newped = {id: uuid.v4(),firstpedido }
-    pedidos.push(newped)
-    return response.status(201).json(newped)
-})
-app.get('/pedidos', (request, response) => {
-    return response.json(pedidos)
-})
+const pedidos = [];
+const pedidEspc = [];
 
-app.put('/alteration/:id', (request, response) => {
-    const { id } = request.params
-    const Mudanca = request.body
-    const pedidmudado = { id, Mudanca }
-    const position = pedidos.findIndex(index => index.id === id)
-    if (position < 0) {
-        return response.status(404).json({ "message": "Id not encontrado" })
-    }
-    pedidos[position] = pedidmudado
-    return response.send("alterado")
-})
+const midle = (request, response, next) => {
+  const { id } = request.params;
+  const position = pedidos.findIndex((pedido) => pedido.id === id);
+  if (position < 0) {
+    return response.status(404).json({ message: "Id não encontrado" });
+  }
+  request.userPosition = position;
+  request.userID = id;
+  next();
+};
 
-app.delete('/delete/:id' ,(request, response)=>{
-    const { id } = request.params
-    const position = pedidos.findIndex(index => index.id === id)
-    if (position < 0) {
-        return response.status(404).json({ "message": "Id não encontrado" })
-    }
-    pedidos.splice(position, 1)
-    return response.status(204).send()
-})
+const midleTwo = (request, response, next) => {
+  const met = request.method;
+  const url = request.url;
+  console.log(`method = ${met} e a nossa url é ${url}`);
+  next();
+};
 
-app.listen(3001)
-app.get('/pedidos/:id', (request, response) => {
-    const { id } = request.params
-    const position = pedidos.findIndex(index => index.id === id)
-    if (position < 0) {
-        return response.status(404).json({ "message": "Id não encontrado" })
-    }
-    pedidEspc.push(pedidos[position]) 
-    return response.json(pedidEspc)
-})
+app.post("/pedido", midleTwo, (request, response) => {
+  const {name , ...mypedido} = request.body;
+  const newped = { id: uuid.v4(), name, ...mypedido};
+  pedidos.push(newped);
+  return response.status(201).json(newped);
+});
 
-app.patch('/status/:id', (request,response)=>{
-    const { id } = request.params
-    const position = pedidos.findIndex(index => index.id === id)
-    if (position < 0) {
-        return response.status(404).json({ "message": "Id não encontrado" })
-    }
-    const Newstatus = pedidos[position] 
-    Newstatus["status"]= "Pronto"
-    return response.status(201).json()
-})
+app.get("/pedidos", (request, response) => {
+  return response.json(pedidos);
+});
+
+app.put("/alteration/:id", midle, (request, response) => {
+  const Mudanca = request.body;
+  const id = request.userID;
+  const position = request.userPosition;
+  const pedidmudado = { id, Mudanca };
+  pedidos[position] = pedidmudado;
+  return response.send("alterado");
+});
+
+app.delete("/delete/:id", (request, response) => {
+  const { id } = request.params;
+  const position = pedidos.findIndex((index) => index.id === id);
+  if (position < 0) {
+    return response.status(404).json({ message: "Id não encontrado" });
+  }
+  pedidos.splice(position, 1);
+  return response.status(204).send();
+});
+
+app.get("/pedidos/:id", midle, (request, response) => {
+  const id = request.userID;
+  const position = request.userPosition;
+  pedidEspc.push(pedidos[position]);
+  return response.json(pedidEspc);
+});
+
+app.patch("/status/:id", (request, response) => {
+  const { id } = request.params;
+  const position = pedidos.findIndex((index) => index.id === id);
+  if (position < 0) {
+    return response.status(404).json({ message: "Id não encontrado" });
+  }
+  const Newstatus = pedidos[position];
+  Newstatus["status"] = "Pronto";
+  return response.json(Newstatus);
+});
+
+app.listen(3001);
